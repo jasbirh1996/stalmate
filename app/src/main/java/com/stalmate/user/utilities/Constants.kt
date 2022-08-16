bf060ec8cb1dfd5955df6a3b7b7b1b847d0e1910
abhay <guptaabhay354@gmail.com>
2022-08-16 15:41:58 +0530

    ajhcjhav

--- app/src/main/java/com/stalmate/user/utilities/Constants.kt
+++ app/src/main/java/com/stalmate/user/utilities/Constants.kt
@@ -6,6 +6,7 @@ object Constants {
     const val URL_SIGN_UP="auth_service/auth_api/user_register"
     const val URL_LOGIN="auth_service/auth_api/user_login"
     const val URL_OTP="auth_service/auth_api/update_password"
+    const val GET_PROFILE_API="auth_service/auth_api/get_profile_data"
 
     const val START_DATE = "01"
 }
