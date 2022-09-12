7d2bbec070be0adb980a1019896610b7449bb8cf
vaibhav <abhishek@teknikoglobal.com>
2022-09-12 15:33:03 +0530

    asdlkasd

---
+++ app/src/main/java/com/stalmate/user/services/MyFirebaseMessagingService.kt
@@ -0,0 +1,142 @@
+package com.mechanicforyou.user.FirebaseNotification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.os.Build
+import android.os.SystemClock
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.stalmate.user.R
+import com.stalmate.user.utilities.Constants
+import com.stalmate.user.view.dashboard.ActivityDashboard
+
+
+/**
+ * Created by Vaibhav
+ */
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    var CHANNEL_ID: String = Constants.CHANNEL_ID
+    var name: CharSequence = "Product"
+    var description = "Notifications"
+    override fun onNewToken(s: String) {
+        super.onNewToken(s)
+        Log.d("notii", s)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        val notificationIntent = Intent(Constants.FILTER_NOTIFICATION_BROADCAST)
+        val nData = remoteMessage.data
+        if (nData != null && nData.isNotEmpty()) {
+            Log.d("Notification", "From: $nData")
+            for (key in nData.keys) {
+                notificationIntent.putExtra(key, nData[key])
+            }
+            sendBroadcast(notificationIntent)
+        }
+        sendNotification(remoteMessage)
+    }
+
+    fun sendNotification(remoteMessage: RemoteMessage) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+            mChannel.description = description
+            mChannel.enableLights(true)
+            mChannel.lightColor = Color.RED
+            notificationManager?.createNotificationChannel(mChannel)
+            val intent = Intent(this, ActivityDashboard::class.java)
+            /*           intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP);*/
+            //  intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            if (remoteMessage.data["order_id"] != null) {
+                intent.putExtra("order_id", remoteMessage.data["order_id"])
+            }
+            if (remoteMessage.data["title"] != null) {
+                intent.putExtra("title", remoteMessage.data["title"])
+            }
+            intent.putExtra("notification_type", remoteMessage.data["title"])
+
+            //   intent.setFlags( Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            val pendingIntent =
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            mBuilder.setSmallIcon(R.drawable.add_friend_icon)
+            if (remoteMessage.notification != null) {
+                mBuilder.setContentTitle(remoteMessage.notification!!.title)
+                mBuilder.setContentText(remoteMessage.notification!!.body)
+            } else {
+                mBuilder.setContentTitle(remoteMessage.data["title"])
+                mBuilder.setContentText(remoteMessage.data["test"])
+            }
+            mBuilder.color = getColor(R.color.colorLightGray)
+            mBuilder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+            mBuilder.priority = NotificationCompat.PRIORITY_DEFAULT
+            mBuilder.setContentIntent(pendingIntent)
+            mBuilder.setCategory(NotificationCompat.CATEGORY_PROMO)
+            mBuilder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            mBuilder.setSound(defaultSoundUri)
+            mBuilder.setAutoCancel(true)
+            val notificationManagerr = NotificationManagerCompat.from(this)
+            notificationManagerr.notify(
+                SystemClock.currentThreadTimeMillis().toInt(),
+                mBuilder.build()
+            )
+        } else {
+            val notificationIntent = Intent(Constants.FILTER_NOTIFICATION_BROADCAST)
+            val nData = remoteMessage.data
+            if (nData != null && nData.isNotEmpty()) {
+                Log.d("Notification", "From: $nData")
+                for (key in nData.keys) {
+                    notificationIntent.putExtra(key, nData[key])
+                }
+                Log.d("notiii", "smaller")
+                sendBroadcast(notificationIntent)
+            }
+
+            // Create an explicit intent for an Activity in your app
+            val intent = Intent(this, ActivityDashboard::class.java)
+            intent.putExtra("notification_type", remoteMessage.data["notification_type"])
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            val pendingIntent =
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+            val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val mBuilder = NotificationCompat.Builder(this)
+            mBuilder.setSmallIcon(R.mipmap.ic_launcher)
+            if (remoteMessage.notification != null) {
+                mBuilder.setContentTitle(remoteMessage.notification!!.title)
+                mBuilder.setContentText(remoteMessage.notification!!.body)
+            } else {
+                mBuilder.setContentTitle(remoteMessage.data["title"])
+                mBuilder.setContentText(remoteMessage.data["body"])
+            }
+            mBuilder.color = resources.getColor(R.color.colorLightGray)
+            mBuilder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+            mBuilder.priority = NotificationCompat.PRIORITY_DEFAULT
+            mBuilder.setContentIntent(pendingIntent)
+            mBuilder.setCategory(NotificationCompat.CATEGORY_PROMO)
+            mBuilder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            mBuilder.setSound(defaultSoundUri)
+            mBuilder.setAutoCancel(true)
+            val notificationManager = NotificationManagerCompat.from(this)
+            notificationManager.notify(
+                SystemClock.currentThreadTimeMillis().toInt(),
+                mBuilder.build()
+            )
+        }
+    }
+}
--- app/src/main/java/com/stalmate/user/view/dashboard/welcome/ActivityWelcome.kt
+++ app/src/main/java/com/stalmate/user/view/dashboard/welcome/ActivityWelcome.kt
@@ -15,9 +15,8 @@ import com.stalmate.user.base.BaseActivity
 import com.stalmate.user.databinding.ActivityWelcomeBinding
 
 
-class ActivityWelcome : BaseActivity(){
-    lateinit var binding:ActivityWelcomeBinding
-    var current_position = 0
+class ActivityWelcome : BaseActivity() {
+    lateinit var binding: ActivityWelcomeBinding
     var count = 0
     override fun onClick(viewId: Int, view: View?) {
 
@@ -25,55 +24,111 @@ class ActivityWelcome : BaseActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_welcome)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome)
 
-       var pagerAdapter=MyPagerAdapter(supportFragmentManager)
+        var pagerAdapter = MyPagerAdapter(supportFragmentManager)
         binding.viewpager.adapter = pagerAdapter
 
         binding.indicator.setViewPager(binding.viewpager)
 
-        current_position = binding.viewpager.currentItem
+        count = binding.viewpager.currentItem
 
         binding.viewpager.setOnTouchListener(OnTouchListener { v, event -> true })
-        binding.viewpager.offscreenPageLimit=0
+        binding.viewpager.offscreenPageLimit = 0
+
+
+
+        binding.toolbar.topAppBar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+
 
         binding.btnNext.setOnClickListener {
 
-            var page=pagerAdapter.getItem(count)
-            Log.d("alskdjas",count.toString())
-            Log.d("alskdjas",page.toString())
-            if (page is FragmentWelcomePage){
+            var page = pagerAdapter.getItem(count)
+
+            if (page is FragmentWelcomePage) {
                 count = +1
-                binding.viewpager.setCurrentItem(count,true)
+                binding.viewpager.setCurrentItem(count, true)
             }
 
-            if (page is FragmentInformationSuggestions){
+            if (page is FragmentInformationSuggestions) {
 
                 count = +1
-                binding.viewpager.setCurrentItem(count,true)
+                binding.viewpager.setCurrentItem(count, true)
 
-                  /*if (page.isValid()){
-                      count = +1
-                      binding.viewpager.setCurrentItem(count,true)
-                  }*/
+                /*if (page.isValid()){
+                    count = +1
+                    binding.viewpager.setCurrentItem(count,true)
+                }*/
             }
         }
+
+
+
+        binding.viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    0 -> {
+                       toolbar(true,"Welcome")
+                    }
+                    1 -> {
+                        toolbar(true,"Welcome")
+                    }
+                   2 -> {
+                       toolbar(false,"Group")
+                    }
+                    3 -> {
+                        toolbar(false,"Pages")
+                    }
+                    4 -> {
+                        toolbar(false,"Events")
+
+                    }
+
+
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
+
 
         /*ToolBar Set*/
-        toolbar()
+        toolbar(true,"Welcome")
 
     }
 
-    private fun toolbar() {
+    private fun toolbar(isCenterVisible:Boolean,text:String) {
 
-        binding.toolbar.toolBarCenterText.visibility = View.VISIBLE
-        binding.toolbar.toolBarCenterText.text = getString(R.string.welcome)
-        binding.toolbar.back.setOnClickListener {
-            onBackPressed()
+        if (isCenterVisible){
+            binding.toolbar.tvhead.visibility = View.GONE
+            binding.toolbar.tvheadCenterHeadBold.visibility=View.VISIBLE
+            binding.toolbar.tvheadCenterHeadBold.text = text
+        }else{
+            binding.toolbar.tvhead.visibility = View.VISIBLE
+            binding.toolbar.tvheadCenterHeadBold.visibility=View.GONE
+            binding.toolbar.tvhead.text = text
         }
+
+
+
+
     }
 
-    class MyPagerAdapter(fragmentManager: FragmentManager?) : FragmentPagerAdapter(fragmentManager!!) {
+    class MyPagerAdapter(fragmentManager: FragmentManager?) :
+        FragmentPagerAdapter(fragmentManager!!) {
         // Returns the fragment to display for that page
         override
         fun getItem(position: Int): Fragment {
@@ -97,9 +152,16 @@ class ActivityWelcome : BaseActivity(){
     }
 
     override fun onBackPressed() {
+
+        if (count != 0) {
+            count--
+            binding.viewpager.setCurrentItem(count, true)
+        } else {
+            super.onBackPressed()
+        }
+
         super.onBackPressed()
     }
-
 
 
 }
