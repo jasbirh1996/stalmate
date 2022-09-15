9ed309965fbb88360b197f0b891776409c9df82d
vaibhav <abhishek@teknikoglobal.com>
2022-09-15 10:35:44 +0530

    asdasd

---
+++ app/src/main/java/com/stalmate/user/view/dashboard/HomeFragment/FragmentSearchListViewModel.kt
@@ -0,0 +1,7 @@
+package com.stalmate.user.view.dashboard.HomeFragment
+
+import androidx.lifecycle.ViewModel
+
+class FragmentSearchListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
