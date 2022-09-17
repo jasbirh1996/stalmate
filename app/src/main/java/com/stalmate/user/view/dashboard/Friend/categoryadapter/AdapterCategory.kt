08b313069d3d83769cf572af1f392709b426b62b
abhay <guptaabhay354@gmail.com>
2022-09-17 16:54:26 +0530

    abhay

--- app/src/main/java/com/stalmate/user/view/dashboard/Friend/categoryadapter/AdapterCategory.kt
+++ app/src/main/java/com/stalmate/user/view/dashboard/Friend/categoryadapter/AdapterCategory.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.slatmate.user.model.Results
 import com.stalmate.user.R
 import com.stalmate.user.databinding.ItemCategoryLayoutBinding
 import com.stalmate.user.view.dashboard.Friend.categorymodel.CategoryResponse
@@ -15,11 +14,11 @@ import com.stalmate.user.viewmodel.AppViewModel
 class AdapterCategory(val viewModel: AppViewModel,
                       val context: Context,
                       var callback: Callbackk
-) : RecyclerView.Adapter<AdapterCategory.ViewHolder>() {
+) : RecyclerView.Adapter<AdapterCategory.CategoryViewHolder>() {
 
     var list = ArrayList<CategoryResponse>()
 
-   inner class ViewHolder(val binding : ItemCategoryLayoutBinding) : RecyclerView.ViewHolder(binding.root){
+   inner class CategoryViewHolder(val binding : ItemCategoryLayoutBinding) : RecyclerView.ViewHolder(binding.root){
 
        @SuppressLint("ResourceAsColor", "ResourceType")
        fun bind(categoryResponse: CategoryResponse) {
@@ -48,16 +47,18 @@ class AdapterCategory(val viewModel: AppViewModel,
         fun onClickDeleteItem(id: String, name : String)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
         var view = LayoutInflater.from(parent.context).inflate(R.layout.item_category_layout, parent, false)
-        return ViewHolder(DataBindingUtil.bind(view)!!)
+        return CategoryViewHolder(DataBindingUtil.bind(view)!!)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
         holder.bind(list.get(position))
     }
 
     override fun getItemCount(): Int {
-       return list.size
+        return list.size
     }
+
+
 }
